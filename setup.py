5dde2ef2f05531bc8fa6eeaabc03a25c30f37c4c
Mrigank Pawagi <mrigankpawagi@gmail.com>
2023-07-06 06:37:48 +0000

    fixe

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     download_url='https://github.com/mutpy/mutpy',
     packages=['mutpy', 'mutpy.operators', 'mutpy.test_runners'],
     package_data={'mutpy': ['templates/*.html']},
-    scripts=['bin/mutpy'],
     install_requires=requirements,
     extras_require={
         'pytest': ["pytest>=3.0"]
